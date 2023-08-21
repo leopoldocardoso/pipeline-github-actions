a2bcbbe66f397696c781b2111318f684127208d1
Leopoldo Cardoso <peixoto.leopoldo@outlook.com>
2023-08-20 21:56:24 -0300

    Criando pipeline com github action

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,7 @@
 resource "aws_instance" "ec2_terraform" {
   ami                         = "ami-053b0d53c279acc90" # us-east-1
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.key.key_name
+  #key_name                    = aws_key_pair.key.key_name
   subnet_id                   = aws_subnet.snet-vpc.id
   vpc_security_group_ids      = [aws_security_group.security_group.id]
   associate_public_ip_address = true
