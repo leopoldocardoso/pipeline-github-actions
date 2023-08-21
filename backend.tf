ff950a8b37fde82ae7bf9c9120dee09e7ba8d0da
Leopoldo Cardoso <peixoto.leopoldo@outlook.com>
2023-08-20 22:45:28 -0300

    Criando pipeline com github action

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "iacterraformtfstate"
-    key    = "estudos-udemy/pipeline-github-actions/terraform.tfstate" #caminho no bucket onde o tfstate será salvo
+    key    = "estudos-udemy/pipeline01-github-actions/terraform.tfstate" #caminho no bucket onde o tfstate será salvo
     region = "us-east-1"
   }
 }
